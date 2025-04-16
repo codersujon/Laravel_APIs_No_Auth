d4c68fddbd2b19d089c08eb4d36ff3c0341610bc
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-04-15 21:08:45 -0700

    Api Controller create and methods settings

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Add Employee POST
+     */
+    public function addEmployee(){
+        
+    }
+
+    /**
+     * List Employee GET
+     */
+    public function listEmployee(){
+
+    }
+
+    /**
+     * Single Employee Details (GET)
+     */
+    public function getSingleEmployee($employeeId){
+
+    }
+
+    /**
+     * Update Employee PUT
+     */
+    public function updateEmployee($employeeId){
+
+    }
+
+    /**
+     * Delete Employee
+     */
+    public function deleteEmployee($employeeId){
+        
+    }
+}
