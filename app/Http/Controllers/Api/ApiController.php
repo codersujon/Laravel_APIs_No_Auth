6902bf1d7f5d1595e1eb8ee4a3839ddff75bacca
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-04-18 12:21:29 -0700

    Add Employee Api End point done

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -11,8 +11,29 @@ class ApiController extends Controller
     /**
      * Add Employee POST
      */
-    public function addEmployee(){
-       
+    public function addEmployee(Request $request){
+
+       // Validation incoming request
+       $request->validate([
+            "name" => "required|string|max:255",
+            "email" => "required|email|unique:employees,email",
+            "age" => "required|integer|min:18|max:100",
+            "gender" => "required|in:male,female,other"
+       ]);
+
+       $employeeObject = new Employee();
+       $employeeObject->name = $request->name;
+       $employeeObject->email = $request->email;
+       $employeeObject->phone_number = $request->phone_number;
+       $employeeObject->age = $request->age;
+       $employeeObject->gender = $request->gender;
+       $employeeObject->save();
+
+       return response()->json([
+            "status" => true,
+            "message" => "Employee saved successfully!"
+       ]);
+
     }
 
     /**
